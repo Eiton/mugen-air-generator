3619394226afe3b527cbf594092855799651bc1a
Eiton <micheal257815@gmail.com>
2020-07-16 14:53:16 +0800

    Updates

---
+++ precache-manifest.4f5908bc32bf8902dba39a835827dd1b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f839ad4db5cd5c55cfee40f2ce3dbb6e",
+    "url": "/mugen-air-generator/index.html"
+  },
+  {
+    "revision": "e52788ad0e97d7abe941",
+    "url": "/mugen-air-generator/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b81782fbd6b5c42c81f9",
+    "url": "/mugen-air-generator/static/js/2.c79a3bfa.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/mugen-air-generator/static/js/2.c79a3bfa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e52788ad0e97d7abe941",
+    "url": "/mugen-air-generator/static/js/main.595981b6.chunk.js"
+  },
+  {
+    "revision": "ee5bb64b87ce999294fa",
+    "url": "/mugen-air-generator/static/js/runtime-main.a0a783de.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mugen-air-generator/static/media/logo.5d5d9eef.svg"
+  }
+]);
